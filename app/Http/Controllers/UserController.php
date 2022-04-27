f3bf84e6eb1e6ce4a43feffdc5ede847ab83c268
ZK <zk27159@gmail.com>
2022-04-27 15:59:11 +0500

    .

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -223,7 +223,7 @@ class UserController extends Controller
     $data = $request->all();
 
     if (!Hash::check($request->old_password, auth()->user()->password)) {
-      return redirect()->back()->with('danger', 'Old Password is Incorrect');
+      return redirect()->back()->with('warning', 'Old Password is Incorrect');
     } else {
       $user = UserModel::find($id);
       $user->password = Hash::make($request->new_password);
