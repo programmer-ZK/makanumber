a502784f70f6e5eac1a42a5a872302cb2d94de10
ZK <zk27159@gmail.com>
2022-04-28 18:21:53 +0500

    .

--- platform/themes/flex-home/layouts/homepage.blade.php
+++ platform/themes/flex-home/layouts/homepage.blade.php
@@ -525,10 +525,10 @@ $pkg = [];
                       @if ($user['avatar_id'])
                       <span class="pl-2 pr-2 userName">{{ $user['first_name'] }}
                         {{ $user['last_name'] }}</span>
-                      <img src="{{ $user->avatar_url }}" style="width:24px; height:24px; border-radius: 50%; " alt="profile-pic" class="mb-1 userName" />
+                      <img src="{{ $user['avatar_url'] }}" style="width:24px; height:24px; border-radius: 50%; " alt="profile-pic" class="mb-1 userName" />
                       @else
                       <span class="pl-2 pr-2 userName">{{ $user['first_name'] }}
-                        {{ $user['last_name'] }}</span> <span class="userName"><span class="icon"><i class="fa fa-user" aria-hidden="true" style="height:34px; border-radius:5px !important;  "></i></span></span>
+                        {{ $user['avatar_url'] }}</span> <span class="userName"><span class="icon"><i class="fa fa-user" aria-hidden="true" style="height:34px; border-radius:5px !important;  "></i></span></span>
                       @endif
                       @endforeach
                     </div>
