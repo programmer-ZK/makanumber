e3ffe6ce4597775aa7c5d547ebab3815295165cc
ZK <zk27159@gmail.com>
2022-04-29 15:24:51 +0500

    .

--- platform/themes/flex-home/layouts/homepage.blade.php
+++ platform/themes/flex-home/layouts/homepage.blade.php
@@ -333,12 +333,13 @@ $pkg = [];
 
                               @if (Auth::user())
                               $user_id = Auth::user()->id;
-                              $user_role_id = DB::table('role_users')->where('user_id', '=', $user_id)->value('role_id');
-                              $documents = \App\Models\Document::where('user_id', $user_id)->get();
 
-                                @if($documents->count() >=1)
-                                      <p>Verified</p>
-                                @endif
+                              $documents = \App\Models\Document::where('user_id','=', $user_id)->get();
+
+                              @if($documents->count() >=1)
+                              <p>Verified</p>
+                              @endif
+
                               @endif
                             </span>
                           </a></p>
