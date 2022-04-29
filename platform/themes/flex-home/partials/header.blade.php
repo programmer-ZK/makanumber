afd1e4f1d7416762cbd470e53a852091d2f68b66
ZK <zk27159@gmail.com>
2022-04-29 16:12:17 +0500

    .

--- platform/themes/flex-home/partials/header.blade.php
+++ platform/themes/flex-home/partials/header.blade.php
@@ -34,7 +34,9 @@ if (Auth::user()) {
 
   <!-- Font Aweosme CDN -->
   <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/js/all.min.js"></script> -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" />
+  <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" /> -->
+  <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
+    integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
 
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat-Bold">
 
