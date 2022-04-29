17ca1db716b0cba8dfdff60435f1ce73efad4e0a
ZK <zk27159@gmail.com>
2022-04-29 16:07:05 +0500

    .

--- platform/themes/flex-home/layouts/homepage.blade.php
+++ platform/themes/flex-home/layouts/homepage.blade.php
@@ -392,7 +392,7 @@ $pkg = [];
                         <span class="pl-2 pr-2 userName">{{ $user['first_name'] }}
                           {{ $user['last_name'] }}
                           @if ($doc_count >= 1)
-                            ✅
+                          <i class="fas fa-badge-check" style="color:#00b4a2 ;"></i>
                           @endif
                         </span>
                         <img src="{{ asset('public/storage/users/' . $user['avatar_id']) }}" style="width:24px; height:24px; border-radius: 50%; " alt="profile-pic" class="mb-1 userName" />
