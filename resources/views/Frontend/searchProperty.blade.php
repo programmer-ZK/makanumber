c0e5ea885a608cb969360cf32a59fb206ce47383
ZK <zk27159@gmail.com>
2022-05-06 14:47:14 +0500

    .

--- resources/views/Frontend/searchProperty.blade.php
+++ resources/views/Frontend/searchProperty.blade.php
@@ -148,9 +148,9 @@ if ($propPurpose == "renting") {
               <span class="pl-2 pr-2" style="float:right !important; text-align:right !important;">
                 {{$user['first_name']}}
                 {{$user['last_name']}}
-                <!-- @if ($doc_count >= 1) -->
+                @if ($doc_count >= 1)
                 <i class="fas fa-badge-check" aria-hidden="true" style="color:#00b4a2;"></i>
-                <!-- @endif -->
+                @endif
               </span>
               <img src="{{asset('public/storage/users/'.$user['avatar_id'])}}" style="width:24px; height:24px; border-radius: 50%; float:right !important; text-align:right !important; " class="mb-1" />
               @else
